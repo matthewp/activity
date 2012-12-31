d14b7f108d90c65bb5c9329c440053ac654a7fd7
Matthew <matthew@phillipsoft.biz>
2012-12-31 16:02:37 -0500

    First commit

---
+++ Readme.md
@@ -0,0 +1,17 @@
+# PRERELEASE - Do not use!
+
+# activity
+
+  s
+
+## Installation
+
+    $ component install matthewp/activity
+
+## API
+
+   
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,3 @@
+exports.ActionBar = require('./lib/action_bar');
+exports.Activity = require('./lib/activity');
+exports.ActivityManager = require('./lib/activity_manager');
